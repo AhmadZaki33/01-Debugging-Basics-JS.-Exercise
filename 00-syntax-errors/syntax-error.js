a1920e676facd40b9923a7b57c7e3b5f08f322ef
Ahmad Zaki <ahmadzaki33@hotmail.com>
2020-09-16 16:32:05 +0200

    Q&A

---
+++ 00-syntax-errors/syntax-error.js
@@ -0,0 +1,15 @@
+
+debugger;
+const hi = 'hi';
+const bye = 'bye';
+const world = 'world';
+
+function helloWorld() {
+  return `${hi} ${world}`;
+}
+
+const goodbyeWorld = {
+  good: 'good',
+  bye,
+  world
+}
---
+++ 01-reference-error/reference-error.js
@@ -0,0 +1,12 @@
+const france = 'france';
+const marsielle = france;
+
+const vacationSpots = (spot1, spot2) => {
+
+  if (spot1.length > spot2.length) {
+    return spot1;
+  }
+
+  return spot2;
+
+}
